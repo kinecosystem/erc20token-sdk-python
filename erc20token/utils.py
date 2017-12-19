98f7fe1d9f08b986684b09dde0c9ca266c1d4b27
David Bolshoy <bolshoy@gmail.com>
2017-12-19 11:31:40 +0200

    catch exception in filter thread runner
    keyfile is now created with tighter permissions as suggested in
    https://github.com/kinfoundation/kin-sdk-python/issues/12

--- erc20token/utils.py
+++ erc20token/utils.py
@@ -3,7 +3,11 @@
 # Copyright (C) 2017 Kin Foundation
 
 import json
+import os
 import sys
+
+import logging
+logger = logging.getLogger(__name__)
 
 
 def create_keyfile(private_key, password, filename):
@@ -16,14 +20,21 @@ def create_keyfile(private_key, password, filename):
 
     :param str filename: keyfile path
 
-    :raises: NotImplementedError: when using Python 2
+    :raises: NotImplementedError: when using Python 3
     """
     if sys.version_info.major >= 3:
         raise NotImplementedError('keyfile creation is only supported in python2')
     from ethereum.tools import keys
     keyfile_json = keys.make_keystore_json(private_key.encode(), password, kdf='scrypt')
-    with open(filename, 'w+') as f:
-        json.dump(keyfile_json, f)
+    try:
+        oldumask = os.umask(0)
+        fd = os.open(filename, os.O_WRONLY | os.O_CREAT | os.O_EXCL, 0o600)
+        with os.fdopen(fd, "w") as f:
+            json.dump(keyfile_json, f)
+    except IOError as e:
+        logger.exception(e)
+    finally:
+        os.umask(oldumask)
 
 
 def load_keyfile(keyfile, password):
@@ -36,7 +47,7 @@ def load_keyfile(keyfile, password):
     :returns: private key
     :rtype: str
 
-    :raises: NotImplementedError: when using Python 2
+    :raises: NotImplementedError: when using Python 3
     :raises: IOError: if the file is not found
     :raises: ValueError: if the keyfile format is invalid
     """
@@ -44,8 +55,8 @@ def load_keyfile(keyfile, password):
         raise NotImplementedError('keyfile usage is only supported in python2')
     with open(keyfile, 'r') as f:
         keystore = json.load(f)
-        from ethereum.tools import keys as ekeys
-        if not ekeys.check_keystore_json(keystore):
+        from ethereum.tools import keys as keys
+        if not keys.check_keystore_json(keystore):
             raise ValueError('invalid keyfile format')
-        return ekeys.decode_keystore_json(keystore, password)
+        return keys.decode_keystore_json(keystore, password)
 
